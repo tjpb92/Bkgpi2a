c66dfe89bf40715344ec543327c43b106b90450f
Thierry Baribaud <thierry.baribaud@gmail.com>
2016-11-21 00:09:53 +0100

    Evolutions au 20/11/16, versin 0.27
    - Modifications mineures sur LastRun et LastRunDAO

--- Bkgpi2a/src/bkgpi2a/LastRun.java
+++ Bkgpi2a/src/bkgpi2a/LastRun.java
@@ -1,12 +1,41 @@
 package bkgpi2a;
 
+import com.fasterxml.jackson.annotation.JsonGetter;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonSetter;
+
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import org.joda.time.format.DateTimeFormatter;
+import org.joda.time.format.ISODateTimeFormat;
+
 /**
  * Classse donnant la dernière date d'exécution d'un programme donné
  *
  * @author Thierry Baribaud
- * @version 0.26
+ * @version 0.27
  */
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class LastRun {
+
+    /**
+     * Constructeur principal de la classe
+     */
+    public LastRun() {
+    }
+
+    /**
+     * Constructeur secondaire de la classe
+     *
+     * @param programName nom du programme
+     */
+    public LastRun(@JsonProperty("programName") String programName) {
+        DateTimeFormatter fmt = ISODateTimeFormat.dateTimeNoMillis();
+
+        this.programName = programName;
+        this.lastRun = fmt.print(new DateTime().toDateTime(DateTimeZone.UTC));
+    }
 
     /**
      * Nom du programme
@@ -21,6 +50,7 @@ public class LastRun {
     /**
      * @return le nom du programme
      */
+    @JsonGetter("programName")
     public String getProgramName() {
         return programName;
     }
@@ -28,6 +58,7 @@ public class LastRun {
     /**
      * @param programName définit le nom du programme
      */
+    @JsonSetter("programName")
     public void setProgramName(String programName) {
         this.programName = programName;
     }
@@ -35,6 +66,7 @@ public class LastRun {
     /**
      * @return la date de dernière exécution
      */
+    @JsonGetter("lastRun")
     public String getLastRun() {
         return lastRun;
     }
@@ -42,6 +74,7 @@ public class LastRun {
     /**
      * @param lastRun définit la date de dernière exécution
      */
+    @JsonSetter("lastRun")
     public void setLastRun(String lastRun) {
         this.lastRun = lastRun;
     }
--- Bkgpi2a/src/bkgpi2a/LastRunDAO.java
+++ Bkgpi2a/src/bkgpi2a/LastRunDAO.java
@@ -1,9 +1,12 @@
 package bkgpi2a;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mongodb.BasicDBObject;
+import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.result.UpdateResult;
+import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.bson.Document;
@@ -12,7 +15,7 @@ import org.bson.Document;
  * Classe qui implémente le pattern DAO pour la classe LastRun
  *
  * @author Thierry Baribaud
- * @version 0.26
+ * @version 0.27
  */
 public class LastRunDAO extends MongoPatternDAO {
 
@@ -29,6 +32,30 @@ public class LastRunDAO extends MongoPatternDAO {
     @Override
     public Object select() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    /**
+     * Récupére la dernière exécution d'un programme par le nom du programme
+     *
+     * @param programName le nom du programme
+     * @return la dernière exécution du programme
+     */
+    public LastRun find(String programName) {
+        LastRun lastRun = null;
+        MongoCursor<Document> cursor;
+
+        if (programName.length() > 0) {
+            objectMapper = new ObjectMapper();
+            cursor = collection.find(new BasicDBObject("programName", programName)).iterator();
+            if (cursor.hasNext()) {
+                try {
+                    lastRun = objectMapper.readValue(cursor.next().toJson(), LastRun.class);
+                } catch (IOException ex) {
+                    Logger.getLogger(LastRunDAO.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+        return lastRun;
     }
 
     /**
