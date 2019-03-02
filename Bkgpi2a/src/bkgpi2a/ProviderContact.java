0c2c34d78e8044547e24c87c5266722b2f59c77c
Thierry Baribaud <thierry.baribaud@gmail.com>
2019-03-02 21:09:23 +0100

    Evolutions au 02/03/2019 : Branche alternative
    Contournement des versions 0.47 et 0.48 pour corriger un bug avec AssigneeIdentified

--- Bkgpi2a/src/bkgpi2a/ProviderContact.java
+++ Bkgpi2a/src/bkgpi2a/ProviderContact.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonGetter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.fasterxml.jackson.annotation.JsonSetter;
 import com.fasterxml.jackson.annotation.JsonUnwrapped;
 
